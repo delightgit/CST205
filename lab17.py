77408a58673387eb5ea0014b4ea110e9512468a7
Delight <dellee@csumb.edu>
2023-11-01 15:23:10 -0700

    first commit

---
+++ lab17.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+#task2
+@app.route('/')
+def hello():
+    return 'Hello world from Flask!'
+
+#task3
+@app.route('/')
+def t_Test():
+    return render_template('template_1.html')
+
+@app.route('/delight')
+def acronym():
+    return render_template('template_2.html')
